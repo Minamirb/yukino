38d4571d12e33a1a792f3f6ede1050688942de72
yalab <rudeboyjet@gmail.com>
2015-01-25 17:41:07 +0900

    Implement sessions#create for login with twitter.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,12 @@
 class SessionsController < ApplicationController
+  def create
+    auth = Auth.find_or_create_form_auth_hash(auth_hash)
+    session[:auth_id] = auth.id
+    redirect_to root_path
+  end
+
+  private
+  def auth_hash
+    request.env['omniauth.auth']
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'root/index'
+  scope :auth do
+    get ":provider/callback", to: "sessions#create"
+  end
+
+  root 'root#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
